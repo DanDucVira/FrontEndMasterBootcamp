8350862c61286a3c696417e5b5412c6bac6bf451
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-15 14:08:39 -0400

    started javascript lecture in bootcamp

---
+++ javascript/primitives/javascriptBasics.js
@@ -0,0 +1,7 @@
+const myName = "Daniel Ducas-Viramontes" //this is a string
+console.log(myName);
+
+const firstName = "Daniel";
+const lastName = "Ducas-Viramontes";
+const sentence = "Hello " + firstName + " " + lastName + "! How are You?!";
+console.log(sentence);
