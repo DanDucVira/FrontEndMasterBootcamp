02c62cb4ca8618762e5946e210ba722b9e7e0014
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-21 13:04:22 -0400

    finished else if statements

--- javascript/controlFlow-loops/controlflow.js
+++ javascript/controlFlow-loops/controlflow.js
@@ -1,7 +1,7 @@
 //control flow
 const isSkyblue = true;
 
-
+//if statement
 if (isSkyblue) {
     console.log("the sky is blue");
 } else {
@@ -9,8 +9,23 @@ if (isSkyblue) {
 }
 
 //Equality comparisons
+// better to use 3 === then two ==
 const myVariable = 10;
 
 if (2 + 2 === 4) {
     console.log("true");
+} else {
+    console.log("false");
 }
+
+//else if statement
+const friendsAtYourParty = 10;
+if (friendsAtYourParty === 0) {
+    console.log("Cool, now I have all the nachos to myself");
+} else if (friendsAtYourParty <= 4) {
+    console.log("Perfect amount to play some mario kart");
+} else {
+    console.log("Woooooo, play the dance music");
+}
+
+
