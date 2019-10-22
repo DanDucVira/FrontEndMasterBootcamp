e964a7261b8603321650f8f184584565b2c64723
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-22 13:21:13 -0400

    finished builtins lecture

---
+++ javascript/Builtins/builtins.js
@@ -0,0 +1,18 @@
+//Builtins
+
+
+//toLowerCase
+const sentence = "ThIs HaS wEiRd CaSiNg On It";
+console.log(sentence.toLowerCase());
+
+//Math.round
+console.log(Math.round(5.1));
+console.log(Math.floor(5.1));
+console.log(Math.ceil(5.1));
+console.log(Math.max(5.1, 1));
+console.log(Math.min(5.1, 1));
+
+//substr
+const name = "Brian Holt";
+console.log(name.substr(6, 3));
+console.log(name.substring(6, 3));
