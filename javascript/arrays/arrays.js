0532f4f747d1afe8493364da0fa2d4bd2ff28ce5
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-10-24 10:07:48 -0400

    added files for array lecture

---
+++ javascript/arrays/arrays.js
@@ -0,0 +1,17 @@
+//Arrays
+
+const daysOfTheWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
+
+console.log(daysOfTheWeek);
+
+const primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17];
+console.log(primeNumbers.length);
+console.log(primeNumbers.join(" | "));
