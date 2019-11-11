52094343f6497ab1695ed5cea99b076a2744a13d
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-11-11 14:37:52 -0500

    finished looping over Query Results lecture

--- javascript/Dom/dom.js
+++ javascript/Dom/dom.js
@@ -1,2 +1,8 @@
 const redSquare = document.querySelector('.red-square');
 redSquare.style.backgroundColor = 'limegreen';
+
+
+const elementsToChange = document.querySelectorAll('.js-target');
+for (let i = 0; i < elementsToChange.length; i++) {
+    const currentElement = elementsToChange[i];
+    currentElement.innerText = "Modified by JavaScript!";
