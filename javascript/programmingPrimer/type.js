90c1b213195f239c386e182d5b2a43bfcc1c8b9b
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-10-11 09:21:59 -0400

    added file type.js

---
+++ javascript/programmingPrimer/type.js
@@ -0,0 +1,16 @@
+typeof 42 // "Number"
+
+typeof "Daniel" // "String
+
+typeof true // "boolean"
+
+typeof undefined // "undefined"
+
+
+typeof {
+    age: 39
+} // "object"
+
+typeof null // "obejct"
+
+typeof [1, 2, 3] // "object"
