3e43e29f01aec4d6f028c16cba37c5ec7b55bf5c
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-10-11 10:08:00 -0400

    added file ifElse.js

---
+++ javascript/programmingPrimer/ifElse.js
@@ -0,0 +1,14 @@
+let age = 30;
+
+if (age >= 18) {
+    goVote();
+}
+
+
+let isEnrolled = true;
+
+if (isEnrolled) {
+    console.log("Take the class!");
+} else {
+    console.log("Enroll First");
+}
